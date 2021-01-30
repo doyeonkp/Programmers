19ba5d1965b25c6f45bdaebc1cd3cf46a9ebf710
tony <tony@tonyui-MacBookPro.local>
2021-01-31 00:30:27 +0900

    Author added, committer Doyeon M Kim

--- 2019kakaoClaw.py
+++ 2019kakaoClaw.py
@@ -3,6 +3,8 @@
 # 크레인 인형뽑기 게임
 # https://programmers.co.kr/learn/courses/30/lessons/64061?language=python3
 
+# @Author: Doyeon M Kim
+
 # board : [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
 # moves : [1,5,3,5,1,2,1,4]
 # result = 4
--- uncompletedMarathoner.py
+++ uncompletedMarathoner.py
@@ -6,6 +6,9 @@
 # 사용해 볼수 있다. Counter('hello world').most_common()을 하게 되면,
 # 많이 나오는 글자 순으로 딕션너리가 반환되고, most_common(숫자)를 하게되면
 # 숫자만큼 자주 나온 글자를 반환한다
+
+# @Author: Doyeon M Kim
+
 # https://programmers.co.kr/learn/courses/30/lessons/42576
 
 from collections import Counter as C
